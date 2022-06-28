cb3f53d3e3cd287c231f74eb54ca05d5dbe9e6f4
Matt Eland <matt.eland@techelevator.com>
2022-06-28 13:44:07 -0400

    Migrating code to GitHub

---
+++ MeaningOfLifeCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MeaningOfLifeCalculator
+{
+    public class Program
+    {
+        private const int answerToTheQuestion = 42;
+
+        public static void Main()
+        {
+            Console.Write("The answer to the question of life, the universe, and everything is: ");
+
+            Console.WriteLine(answerToTheQuestion);
+        }
+    }
+}
